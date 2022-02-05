dc860872be4300d17ccc51ecfe0746b7ffa1d3d4
patel-zeel <patel_zeel@iitgn.ac.in>
2022-02-05 16:35:53 +0400

    Enable batch support

--- skgpytorch/models/base.py
+++ skgpytorch/models/base.py
@@ -1,6 +1,8 @@
 import torch
 import gpytorch
 import warnings
+import faiss
+import numpy as np
 
 
 class BaseRegressor(torch.nn.Module):
@@ -8,12 +10,24 @@ class BaseRegressor(torch.nn.Module):
         super().__init__()
         self.history = {"train_loss": []}
         self.best_restart = None
+        self.res = faiss.StandardGpuResources()
+
+    def compute_train_nn_idx(self, k):
+        x = (self.train_x.data.float()).cpu().numpy()
+        self.cpu_index = faiss.IndexFlatL2(self.train_x.size(-1))
+        self.gpu_index = faiss.index_cpu_to_gpu(self.res, 1, self.cpu_index)
+        self.gpu_index.add(x)
+        self.train_nn_idx = (
+            torch.from_numpy(self.gpu_index.search(x, k)[1])
+            .long()
+            .to(self.train_x.device)
+        )
 
     def fit(
         self,
-        X=None,
-        y=None,
-        n_iters=1,
+        batch_size=None,
+        lr=0.1,
+        n_epochs=1,
         n_restarts=1,
         verbose=0,
         verbose_gap=1,
@@ -21,28 +35,18 @@ class BaseRegressor(torch.nn.Module):
     ):
         if random_state is not None:
             torch.manual_seed(random_state)
+        if batch_size is None:
+            batch_size = self.train_x.shape[0]
 
-        if X is None:
-            if hasattr(self.model, "train_inputs"):
-                X = self.model.train_inputs[0]
-            else:
-                raise ValueError("X is required.")
-
-        if y is None:
-            if hasattr(self.model, "train_targets"):
-                y = self.model.train_targets
-            else:
-                raise ValueError("y is required.")
-        X = X.to(self.device)
-        y = y.to(self.device)
-        self.model.to(self.device)
         self.model.train()
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.1)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
 
         if len(self.history["train_loss"]) > 0:
             warnings.warn(
                 "Training loss is not empty. This may cause unexpected behavior."
             )
+
+        self.compute_train_nn_idx(k=batch_size)
 
         best_loss = float("inf")
         for restart in range(n_restarts):
@@ -50,21 +54,30 @@ class BaseRegressor(torch.nn.Module):
             for param in self.model.parameters():
                 torch.nn.init.normal_(param, mean=0.0, std=1.0)
 
-            for i in range(n_iters):
-                self.optimizer.zero_grad()
-                loss = self(X, y)
-                loss.backward()
-                if verbose and i % verbose_gap == 0:
-                    print(
-                        "Restart: {}, Iter: {}, Loss: {:.4f}, Best Loss: {:.4f}".format(
-                            restart, i, loss.item(), best_loss
+            for epoch in range(n_epochs):
+                for iteration in range(self.train_x.shape[0] // batch_size):
+                    idx = torch.randint(low=0, high=self.train_x.shape[0], size=(1,))[0]
+                    indices = self.train_nn_idx[idx]
+                    X_batch = self.train_x[indices]
+                    y_batch = self.train_y[indices]
+                    # X_batch = self.train_x
+                    # y_batch = self.train_y
+
+                    self.optimizer.zero_grad()
+                    loss = self(X_batch, y_batch)
+                    loss.backward()
+                    if verbose and epoch % verbose_gap == 0:
+                        print(
+                            "Restart: {}, Iter: {}, Loss: {:.4f}, Best Loss: {:.4f}".format(
+                                restart, epoch, loss.item(), best_loss
+                            )
                         )
-                    )
-                self.history["train_loss"][restart].append(loss.item())
-                self.optimizer.step()
+                    self.history["train_loss"][restart].append(loss.item())
+                    self.optimizer.step()
 
             # Last loss
-            loss = self(X, y)
+            # This can consume entire RAM
+            loss = self(self.train_x, self.train_y)
 
             # Check if best loss
             if loss.item() < best_loss:
--- skgpytorch/models/exact_gp.py
+++ skgpytorch/models/exact_gp.py
@@ -10,21 +10,25 @@ class ExactGPRegressor(BaseRegressor):
     Call the constructor of base class after defining the model.
     """
 
-    def __init__(self, train_x, train_y, kernel, device="cpu"):
+    def __init__(self, train_x, train_y, kernel):
         super().__init__()
         likelihood = gpytorch.likelihoods.GaussianLikelihood()
-        self.model = ExactGPModel(train_x, train_y, likelihood, kernel)
+        self.train_x = train_x
+        self.train_y = train_y
+        self.model = ExactGPModel(self.train_x, self.train_y, likelihood, kernel)
         self.mll = gpytorch.mlls.ExactMarginalLogLikelihood(
             self.model.likelihood, self.model
         )
-        self.device = device
 
     def forward(self, X, y):
+        self.model.set_train_data(X, y, strict=False)
         output = self.model(X)
         return -self.mll(output, y)
 
-    def predict(self, X, y, X_test):
-        self.model.set_train_data(inputs=X.to(self.device), targets=y.to(self.device))
+    def predict(self, X_test):
+        self.model.set_train_data(
+            inputs=self.train_x, targets=self.train_y, strict=False
+        )
         if len(self.history["train_loss"]) < 1:
             warnings.warn(
                 "Model is not fitted yet. This may cause unexpected behavior."
@@ -32,5 +36,5 @@ class ExactGPRegressor(BaseRegressor):
 
         self.model.eval()
         with torch.no_grad(), gpytorch.settings.fast_pred_var():
-            self.pred_dist = self.model.likelihood(self.model(X_test.to(self.device)))
+            self.pred_dist = self.model.likelihood(self.model(X_test))
             return self.pred_dist
